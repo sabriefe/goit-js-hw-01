6a9440667c5b7f4db3fef62abd2f6bb6f5de2890
Sabri_Efe <se.zybk@hotmail.com>
2026-03-18 09:29:33 +0300

    updated

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,13 @@
-'use strick';
+'use strict';
 
 function getElementWidth(content, padding, border){
     const parsedContent = Number.parseFloat(content);
     const parsedPadding = Number.parseFloat(padding); 
     const parsedBorder = Number.parseFloat(border); 
-    const totalWidth = Number.parseInt(parsedContent + (parsedPadding + parsedBorder) * 2);
+    const totalWidth = parsedContent + (parsedPadding + parsedBorder) * 2;
     return totalWidth;
 }
+
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
